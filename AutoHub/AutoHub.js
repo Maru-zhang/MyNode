a43e632a1c07c0892f124ceb79a60a4d603235e3
Maru <zhangbinhui@souche.com>
2017-05-19 10:41:00 +0800

    this is auto commit

--- AutoHub/AutoHub.js
+++ AutoHub/AutoHub.js
@@ -7,7 +7,7 @@ function scheduleCronstyle(){
         console.log('start fake ... \n scheduleCronstyle:' + new Date());
         console.log("sh" + " " + __dirname + '/fake-commit.sh');
         callfile.execFile(__dirname + '/fake-commit.sh', function (err, stdout, stderr) {
-        	console.log(err);
+        	if (err !== null) { console.log("auto commit is successful !"); }
 		});
     }); 
 }
