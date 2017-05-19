f302a149433f7f02bcd81bd24b8fcaf85b7eb9c1
Maru <zhangbinhui@souche.com>
2017-05-19 10:24:26 +0800

    this is auto commit

---
+++ AutoHub/fake-commit.sh
@@ -0,0 +1,12 @@
+# /bin/bash
+
+cd `dirname $0`
+path = "`dirname $0`""/mass.txt"
+if [ ! -x "$path"]; then
+ 	touch mass.txt
+fi
+date >> mass.txt
+cd ..
+git add .
+git commit -m "this is auto commit"
+git push origin
