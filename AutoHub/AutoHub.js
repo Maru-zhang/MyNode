b87fca388d4813e4f2966ce4a9807bffa5592b14
Maru <maru-zhang@foxmail.com>
2017-05-19 10:51:56 +0800

    this is auto commit

--- AutoHub/AutoHub.js
+++ AutoHub/AutoHub.js
@@ -3,7 +3,7 @@ var callfile = require('child_process');
 var path = require('path')
 
 function scheduleCronstyle(){
-    schedule.scheduleJob('0 0 20 * * *', function(){
+    schedule.scheduleJob('0 * * * * *', function(){
         console.log('start fake ... \n scheduleCronstyle:' + new Date());
         console.log("sh" + " " + __dirname + '/fake-commit.sh');
         callfile.execFile(__dirname + '/fake-commit.sh', function (err, stdout, stderr) {
