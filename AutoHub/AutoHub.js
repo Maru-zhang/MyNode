90836780af089bd28496e71df1894351ba12be38
Maru <maru-zhang@foxmail.com>
2017-05-22 10:26:47 +0800

    dsa

--- AutoHub/AutoHub.js
+++ AutoHub/AutoHub.js
@@ -3,12 +3,12 @@ var callfile = require('child_process');
 var path = require('path')
 
 function scheduleCronstyle(){
-    schedule.scheduleJob('0 37 20 * * *', function(){
+    schedule.scheduleJob('* * * * * *', function(){
         console.log('start fake ... \n scheduleCronstyle:' + new Date());
         console.log("sh" + " " + __dirname + '/fake-commit.sh');
         callfile.execFile(__dirname + '/fake-commit.sh', function (err, stdout, stderr) {
         	if (err !== null) { console.log("auto commit is successful !"); }
-		});
+	});
     }); 
 }
 
