8b1c327d2d073337e907343f263cf6d6284eb296
Maru <zhangbinhui@souche.com>
2017-06-27 23:09:30 +0800

    Adjust push strategy.

--- AutoHub/AutoHub.js
+++ AutoHub/AutoHub.js
@@ -2,14 +2,24 @@ var schedule = require('node-schedule');
 var callfile = require('child_process');
 var path = require('path')
 
-function scheduleCronstyle(){
-    schedule.scheduleJob('0 40 15 * * *', function(){
+function excuteTask() {
+    callfile.execFile(__dirname + '/fake-commit.sh', function (err, stdout, stderr) {
+    	if (err !== null) { console.log("auto commit is successful !"); }
+	});
+}
+
+function scheduleCronstyle() {
+    schedule.scheduleJob('0 0 20 * * *', function() {
         console.log('start fake ... \n scheduleCronstyle:' + new Date());
         console.log("sh" + " " + __dirname + '/fake-commit.sh');
-        callfile.execFile(__dirname + '/fake-commit.sh', function (err, stdout, stderr) {
-        	if (err !== null) { console.log("auto commit is successful !"); }
-	});
-    }); 
-}
+        var count = 1;
+        if (Math.ceil(Math.random() * 3) % 2 == 0) {
+        	count++;
+        }
+        for (;count > 0; i--) {
+        	excuteTask();
+        }
+    });
+  }
 
 scheduleCronstyle();
